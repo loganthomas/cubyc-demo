d7402b0a75c50cec884a63fa630d0e30d5178eea
loganthomas <logan.thomas005@gmail.com>
2024-07-11 14:59:24 -0700

    Code change

---
+++ mat_mul.py
@@ -0,0 +1,12 @@
+import numpy as np
+from cubyc import Run
+
+@Run(tags=['linear_algebra'])
+def matrix_multiplication(n_size: int):
+    A = np.random.rand(n_size, n_size)
+    B = np.random.rand(n_size, n_size)
+
+    _ = np.dot(A, B)
+
+for n_size in [5, 7, 9]:
+    matrix_multiplication(n_size=n_size)
